b957a4df2a36775d5b6696344733364977a3b724
Alex Eagleson <alexeagleson@gmail.com>
2018-09-04 21:05:53 -0400

    added user account registration

--- src/App.ts
+++ src/App.ts
@@ -5,6 +5,9 @@ const path = require("path");
 const hbs = require("hbs");
 const bodyParser = require('body-parser')
 const uniqid = require('uniqid');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const publicPath = path.join(__dirname, "public");
 
@@ -30,7 +33,7 @@ class App {
       let words = '';
       const storyArray = storyText.split(' ');
       for (let i = (storyArray.length - numWords); i < storyArray.length; i += 1) {
-        words = `${words} ${storyArray[i]}`;
+        if (!!storyArray[i]) words = `${words} ${storyArray[i]}`;
       }
       return new hbs.SafeString(`<p>${words}</p>`);
     });
@@ -40,7 +43,7 @@ class App {
     const router = express.Router();
 
     router.get("/", (req, res) => {
-      res.status(200).render(publicPath + "/views/index.hbs", {foo: 'bar'});
+      res.status(200).render(publicPath + "/views/index.hbs");
     });
 
     router.get("/create", (req, res) => {
@@ -49,23 +52,85 @@ class App {
         .catch(err => { console.log(err); });
     });
 
-    router.get("/addnew", (req, res) => {
+    router.get("/addstory", (req, res) => {
       this.sql.runQuery(`SELECT * FROM story_portions ORDER BY date_added DESC LIMIT 1`)
         .then(result => {
-          res.status(200).render(publicPath + "/views/index.hbs", { databaseResult: result });
+          res.status(200).render(publicPath + "/views/addstory.hbs", { databaseResult: result });
         })
         .catch(err => {
           console.log(err);
         });
     });
 
-    router.post("/submitstory", (req, res) => {
+    router.get("/fullstory", (req, res) => {
+      this.sql.runQuery(`SELECT * FROM story_portions`)
+        .then(result => {
+          res.status(200).render(publicPath + "/views/fullstory.hbs", { databaseResult: result });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+
+    router.get("/login", (req, res) => {
+      res.status(200).render(publicPath + "/views/login.hbs");
+    });
+
+    router.get("/register", (req, res) => {
+      res.status(200).render(publicPath + "/views/register.hbs");
+    });
+
+    // POST
+
+    router.post("/addstory", (req, res) => {
       const currentDateAndTime = new Date().toISOString().split("T");
       const currentDateAndTimeSqlFormat = `${currentDateAndTime[0]} ${currentDateAndTime[1].split(".")[0]}`;
 
       this.sql.runQuery(`INSERT INTO story_portions (story_id, story_text, num_words, date_added) VALUES ('${uniqid()}', '${req.body.story}', ${req.body.numWords}, '${currentDateAndTimeSqlFormat}')`)
         .then(result => {
           res.status(200).render(publicPath + "/views/index.hbs");
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+
+    router.post("/login", (req, res) => {
+      this.sql.runQuery(`SELECT password FROM users WHERE username = '${req.body.username}'`)
+        .then(result => {
+          if (result.length > 0) {
+            bcrypt.compare(req.body.password, result[0].password).then((compareResult) => {
+              if (compareResult) {
+                res.status(200).render(publicPath + "/views/index.hbs", { status: `Logged in as: ${req.body.username}.` });
+              } else {
+                res.status(200).render(publicPath + "/views/login.hbs", { status: 'Password did not match.' });
+              }
+            });
+          } else {
+            res.status(200).render(publicPath + "/views/login.hbs", { status: 'Username not found.' });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+
+    router.post("/register", (req, res) => {
+      this.sql.runQuery(`SELECT username FROM users WHERE username = '${req.body.username}'`)
+        .then(result => {
+          if (result.length > 0) {
+            res.status(200).render(publicPath + "/views/register.hbs", { status: 'Username already registered.' });
+          } else {
+            bcrypt.hash(req.body.password, saltRounds).then((hash) => {
+              this.sql.runQuery(`INSERT INTO users (username, password) VALUES ('${req.body.username}', '${hash}')`)
+              .then(result => {
+                res.status(200).render(publicPath + "/views/index.hbs", { status: `Registered user: ${req.body.username}.` });
+              })
+              .catch(err => {
+                console.log(err);
+              });
+            });
+          }
         })
         .catch(err => {
           console.log(err);
